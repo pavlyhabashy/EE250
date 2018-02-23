2dbb7bec44cbea57809b7895e705d0f36d7d7ac7
Pavly Habashy <phabashy@usc.edu>
2018-02-23 15:19:18 -0800

    Fixed LED

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -3,10 +3,13 @@ import Adafruit_GPIO.SPI as SPI
 import time
 
 def Main()
+	LED = 12
+	GPIO.setup(LED, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(LED, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	while True:
-		GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.output(LED, 1)
 		time.sleep(0.5)
-		GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.output(LED, 0)
 		time.sleep(0.5)
 
 if __name__ == '__main__':
