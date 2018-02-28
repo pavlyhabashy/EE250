e14e009121bc8c760720b282396365c6226062a0
Pavly Habashy <phabashy@usc.edu>
2018-02-27 16:45:09 -0800

    Added simpleTest

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,9 +1,21 @@
 import RPi.GPIO as GPIO
+import Adafruit_MCP3008
+import Adafruit_GPIO.SPI as SPI
 import time
 
 LED = 11
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED, GPIO.OUT)
+
+# Software SPI configuration:
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+print('-' * 57)
 
 def Main():
 	
@@ -12,6 +24,16 @@ def Main():
 		time.sleep(0.5)
 		GPIO.output(LED, GPIO.LOW)
 		time.sleep(0.5)
+
+		# Read all the ADC channel values in a list.
+	    values = [0]*8
+	    for i in range(8):
+	        # The read_adc function will get the value of the specified channel (0-7).
+	        values[i] = mcp.read_adc(i)
+	    # Print the ADC values.
+	    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+	    # Pause for half a second.
+	    time.sleep(0.5)
 
 if __name__ == '__main__':
     Main()
